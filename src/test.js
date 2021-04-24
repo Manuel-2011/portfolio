6472e7a0cb41a26ce7f5ea64a83dd95f2e84f9dd
Manuel <mosquera.manuel2011@gmail.com>
2021-04-23 19:36:43 -0500

    :construction: setting up webpack and babel

---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Test2 from "./test2";
+
+export default function () {
+  return (
+    <>
+      <h1>Hola</h1> <Test2 />
+    </>
+  );
+}
---
+++ src/test2.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function () {
+  return <h1>Hola test 2</h1>;
+}
