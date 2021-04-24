717a14d361422b0534b047974888fd816bf78f1b
Manuel <mosquera.manuel2011@gmail.com>
2021-04-24 13:07:21 -0500

    :wrench: material ui added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Paper } from '@material-ui/core';
 
 const App = () => {
-  return <h1>The App is working! &#128516; &#128075;</h1>;
+  return (
+    <Paper>
+      <h1>The App is working! &#128516; &#128075;</h1>
+    </Paper>
+  );
 };
 
 export default App;
